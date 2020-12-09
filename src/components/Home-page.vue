698abdadbdda51c1db6bc09c83f4f094d59c57e1
MartaStavreska <stavreskamarta@gmail.com>
2020-12-09 17:22:36 +0100

    'change'

--- src/components/Home-page.vue
+++ src/components/Home-page.vue
@@ -2,18 +2,18 @@
 <div>
 <HomeHeader />
 <div class="main-search text-center">
-    <img src="../../logotext.png" class="logo-img mr-5 col-md-3" alt="">
+    <img src="../../logotext.png" class="logo-img mr-5 col-md-3 m-3" alt="">
     <div class="row">
         <div class="container text-center">
             <div class="row">
                 <div class="col-6 offset-3">
-                    <div class="search input-group mb-3">
+                    <div class="search input-group mb-4">
                         <span class="input-group-text search-icon" id="basic-addon1"><i class="fa fa-search"></i></span>
                         <input id="search-input" type="text" class="form-control" placeholder="Search">
                     </div>
-                    <div class="text-center">
-                        <button class="btn btn-info mr-2 btn"> <span class="btn-text">Search</span> </button>
-                        <button class="btn btn-info mr-2 btn"> <span class="btn-text">Daily Luck</span> </button>
+                    <div class="text-center m-2">
+                        <button class="btn btn-light mr-2 btn "> <span class="btn-text p-1">Search</span> </button>
+                        <button class="btn btn-light mr-2 btn "> <span class="btn-text p-1">Daily Luck</span> </button>
                     </div>
                 </div>
             </div>
@@ -65,6 +65,10 @@ export default {
     text-align: center;
     pointer-events: none;
     color: #aaa;
+}
+.btn{
+    width: 150px;
+    background-color:  rgb(227, 232, 236);
 }
 @media only screen and (max-width: 600px) {
   img {
