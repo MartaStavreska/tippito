54430e2df0be3a25c1e2fc7dc3c951570d300f55
MartaStavreska <stavreskamarta@gmail.com>
2020-12-12 14:34:37 +0100

    'change'

--- src/pages/Modal.vue
+++ src/pages/Modal.vue
@@ -3,9 +3,9 @@
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
-      <div class="modal-header">
+      <div class="modal-header border-0">
         <h5 class="modal-title" id="exampleModalLabel"></h5>
-        <span class="text-secondary font-size-14">Full Name</span>
+        <span class="text-secondary font-size-13">Full Name</span>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -14,7 +14,8 @@
       <div class="modal-body text-center">
         <h6 class="blue-color font-weight-bold">Locomotiva-Moscow - FC Bayern Munchen (Tip)</h6>
         <p class="score" style="font-size:29px"><strong> 2 / 2 & 3 +</strong></p>
-        <p class="text-secondary font-size-10">Published on  <br/> 28.10.2020 13:10</p>
+        <p class="text-secondary font-size-10">Published on  <br/>
+         <span class="font-size-10">28.10.2020 13:10</span> </p>
         <p class="text-secondary"> <strong class="font-size-12"> WIN/LOSE
         <br>RESULT</strong> 
         </p>
@@ -27,5 +28,8 @@
 <style>
 .modal{
   padding-top:10%;
+}
+.modal-content{
+  border-radius: 1rem;
 }
 </style>
